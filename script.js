411e1ad132506bcb7c736db5c2e761ce15a9e9a4
OchsaufLerchenau <maciej.burdzy@gmail.com>
2020-04-16 14:49:59 +0200

    project finished

--- script.js
+++ script.js
@@ -25,7 +25,27 @@ function makeArray () {
     }
 }
 
-function hoverColors() {  
+function toHexConverter (int) {
+    var hexa = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+    if (int < 10) {
+        return int;
+    } else {
+        return hexa[int];
+    }
+}
+
+function buttonRandomColor() {
+    const btn = document.getElementById('btn1');
+    btn.addEventListener('mouseenter', (e) => {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        e.target.style.backgroundColor = '#' + randomColor;
+    });
+    btn.addEventListener('mouseleave', (e) => {
+        e.target.style.backgroundColor = 'rgb(220, 220 , 220)';
+    });
+}
+
+function hoverColors() { 
     for (i = 0; i < elements.length; i++) {  
         elements[i].addEventListener('mouseenter', function (e) { 
             const randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -37,28 +57,16 @@ function hoverColors() {
     }   
 }
 
-function toHexConverter (int) {
-    var hexa = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-    if (int < 10) {
-        return int;
-    } else {
-        return hexa[int];
-    }
-}
-
-
 function hoverGrays() {
     for (i = 0; i < elements.length; i++) {
         elements[i].classList.add('13');
         elements[i].addEventListener('mouseenter', function (e) { 
-           
             e.target.style.borderRadius = '35%';
             var counter = e.target.getAttribute('class');
             hex = toHexConverter(counter);
             if (counter >= 0) {
                 var grayScale = '#' + hex + hex + hex;
                 e.target.style.backgroundColor = grayScale;
-
                 e.target.setAttribute('class', counter-2)   
             }
         });
@@ -72,6 +80,7 @@ const container = document.getElementById('container');
 window.onload = promptForGridSize()
 window.onload = createGrid();
 window.onload = makeArray();
-//window.onload = hover()
+window.onload = buttonRandomColor();
+
 
 
